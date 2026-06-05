57901b4e274b0d654da24b4666526f231750fb25
khushisarojj@gmail.com <khushisarojj@gmail.com>
2026-06-05 17:25:37 +0530

    fix: WebRTC STUN only + HTML5 audio autoplay

--- main.py
+++ main.py
@@ -206,22 +206,29 @@ def main():
             key="exercise-analysis",
             mode=WebRtcMode.SENDRECV,
             video_processor_factory=VideoProcessorClass,
+            # rtc_configuration={
+            #     "iceServers": [
+            #         {"urls": ["stun:stun.l.google.com:19302"]},
+            #         {"urls": ["stun:stun1.l.google.com:19302"]},
+            #         {
+            #             "urls": ["turn:openrelay.metered.ca:80"],
+            #             "username": "openrelayproject",
+            #             "credential": "openrelayproject",
+            #         },
+            #         {
+            #             "urls": ["turn:openrelay.metered.ca:443"],
+            #             "username": "openrelayproject",
+            #             "credential": "openrelayproject",
+            #         },
+            #     ]
+            # },
             rtc_configuration={
                 "iceServers": [
                     {"urls": ["stun:stun.l.google.com:19302"]},
                     {"urls": ["stun:stun1.l.google.com:19302"]},
-                    {
-                        "urls": ["turn:openrelay.metered.ca:80"],
-                        "username": "openrelayproject",
-                        "credential": "openrelayproject",
-                    },
-                    {
-                        "urls": ["turn:openrelay.metered.ca:443"],
-                        "username": "openrelayproject",
-                        "credential": "openrelayproject",
-                    },
+                    {"urls": ["stun:stun2.l.google.com:19302"]},
                 ]
-            },
+            }
             media_stream_constraints={"video": True, "audio": False},
             async_processing=True
         )
